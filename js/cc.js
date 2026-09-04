67a0e84d0b51574fe50a3ea19048bfc20e95d3cd
cv <shahzadrehman869@gmail.com>
2026-09-04 13:09:39 +0200

    Update cc.js

--- js/cc.js
+++ js/cc.js
@@ -1,11 +1,17 @@
 <script>
+document.addEventListener("contextmenu", function (e) {
+    e.preventDefault();
+});
+
 document.addEventListener("keydown", function (e) {
     if (
         e.key === "F12" ||
         (e.ctrlKey && e.shiftKey && ["I", "J", "C"].includes(e.key.toUpperCase())) ||
-        (e.ctrlKey && e.key.toUpperCase() === "U")
+        (e.ctrlKey && e.key.toUpperCase() === "U") ||
+        (e.metaKey && e.altKey && e.key.toUpperCase() === "I")
     ) {
         e.preventDefault();
+        e.stopPropagation();
     }
 });
 </script>
